575b865f1fc6ec3eb2cc3cd71a9abab5d61d6536
Choi Seo Yeon <serah0716@sookmyung.ac.kr>
2023-06-14 23:42:34 +0900

    12. @RequestBody 사용하기

--- demo/src/main/java/com/example/demo/dto/TestRequestBodyDTO.java
+++ demo/src/main/java/com/example/demo/dto/TestRequestBodyDTO.java
@@ -1,5 +1,7 @@
 package com.example.demo.dto;
-
+import lombok.Data;
+@Data // 에러가 발생하는 경우 Ctrl+1 import . 키를 누른 후 맨 위에 나오는 를 선택한다
 public class TestRequestBodyDTO {
-
+private int id;
+private String message;
 }
